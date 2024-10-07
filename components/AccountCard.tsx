e8d81cb3a2086dbd5e24e6aa81b6c95b97f12867
nxiiun <nxiiun@gmail.com>
2024-10-08 11:40:48 +1300

    fix

--- components/AccountCard.tsx
+++ components/AccountCard.tsx
@@ -48,16 +48,18 @@ export const AccountCard = ({ balance, className }: { balance?: bigint | number 
           <div className="text-sm font-semibold">Current account</div>
           <div className="flex items-center gap-2">
             <div className="text-xs text-muted-foreground translate-y-[0.5px]">Balance</div>
-            <div className="text-sm font-medium">{formatBalance(usdcBalance, { decimals: 6, postfix: ' USDC' })}</div>
+            <div className="text-sm">{formatBalance(usdcBalance, { decimals: 6, postfix: ' USDC' })}</div>
           </div>
         </div>
       </div>
       <div className={cn('rounded-t-lg px-3 py-2 bg-gradient3 h-16 relative z-30', '-mt-8 max-h-10 overflow-hidden')}>
         <div className="flex justify-between">
           <div className="text-sm font-semibold">Spending account</div>
-          <div className="text-sm">
-            <div className="text-muted-foreground">Balance</div>
-            <div>{balance}</div>
+          <div className="flex items-center gap-2">
+            <div className="text-xs text-muted-foreground translate-y-[1px]">Linked</div>
+            <div className="text-sm">
+              {formatBalance(usdcBalance, { prefix: 'Spend up to ', decimals: 6, postfix: ' USDC' })}
+            </div>
           </div>
         </div>
       </div>
