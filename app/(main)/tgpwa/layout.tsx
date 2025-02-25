401f945998c345c81ca7e57ad0aac83e5eb1515c
nxiiun <nxiiun@gmail.com>
2025-02-25 19:00:37 +1300

    update full

--- app/(main)/tgpwa/layout.tsx
+++ app/(main)/tgpwa/layout.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useEffect } from 'react';
+
 import { Deposit } from './components/Deposit';
 import { Login } from './components/Login';
 import { Pay } from './components/Pay';
@@ -6,6 +8,19 @@ import { TonWallet } from './components/TonWallet';
 import './telegram-web-app';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const webview = (window as any).TelegramWebviewProxy;
+
+      if (webview) {
+        webview.postEvent('web_app_request_fullscreen');
+        // tg.enableClosingConfirmation();
+        // tg.setBackgroundColor('#ffffff');
+        // tg.setHeaderColor('#000000');
+      }
+    }
+  }, []);
+
   return (
     <>
       <main className="h-full">{children}</main>
